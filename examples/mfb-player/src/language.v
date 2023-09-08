6dbd9af727de44150247dcc006013f05cb9b7566
Mehmet Ali Şipi <mehmetalisipi@gmail.com>
2023-09-08 20:42:37 +0300

    Improve text drawing system and examples

--- examples/mfb-player/src/language.v
+++ examples/mfb-player/src/language.v
@@ -1,8 +1,25 @@
 module main
 
 const (
-	lp_nothing_playing = "Nothing is playing"
-	lp_filter_media = "Filter the Media"
-	lp_videos = "Videos"
-	lp_music = "Music"
+	language_pack = load_language_pack()
 )
+
+fn load_language_pack () map[string]string {
+	$if turkish? {
+		return {
+			"nothing_playing": "Medya oynatılmıyor"
+			"filter_media": "Medyayı filtrele"
+			"videos": "Videolar"
+			"music": "Müzik"
+			"media_title": "Medyanın Başlığı"
+		}
+	}
+
+	return { // default language pack
+		"nothing_playing": "Nothing is playing"
+		"filter_media": "Filter the Media"
+		"videos": "Videos"
+		"music": "Music"
+		"media_title": "Media title"
+	}
+}
